74e4ebd76c258e5db47a8cd0081ccc72f8c6c70c
rgonsahn <rgonsahn@yahoo.com>
2022-12-07 13:09:08 -0500

    debugged userControllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,53 +1,88 @@
-const {Schema,model,Types}= require('mongoose');
+const {User,Opinion}= require("../models");
 
-// schema that creates the user model 
-
-const userSchema = new Schema (
-{
-
-username:{
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+module.exports ={
+//  get users 
+getUser(req, res) {
+    User.find({})
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err))
 },
-email:{
-    type: String,
-    unique: true,
-    required: true,
-    match: [ /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Please enter a valid email address"],
+// get one user
+getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+    .populate("opinions")
+    .populate("friends")
+    .select("-__v")
+    .then((user) =>
+    !user
+    ? res.status(404).json({ message: "No User found with this ID!"})
+    : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+//  create a user
+createUser(req, res) {
+    User.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+    });
+},
+// update a user 
+updateUser(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+    )
+    .then((user) =>
+    !user
+    ? res.status(404).json({ message: "No User found with that ID!" })
+    : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+// delete user 
+deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+    .then((user) =>
+    !user
+    ? res.status(404).json({ message: "No User found with this ID!" })
+    : Opinion.deleteMany({ _id: { $in: user.opinions }})
+    )
+    .then(() => res.json({ message: "User and Opinion deleted!" }))
+    .catch((err) => res.status(500).json(err));
+},
+//add friend
 
-    opinions: [
-    {
-        type: Schema.Types.ObjectId,
-        ref: "Opinion",
-    },
-],
-
-friends:[
-    {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-],
+addFriend(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId }},
+        { runValidators: true, new: true }
+    )
+    .then((user) =>
+    !user
+    ? res.status(404).json({ message: "No User found with this ID!" })
+    : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+},
+deleteFriend(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId }},
+        { new: true }
+    )
+    .then(
+        (user) =>
+        !user
+        ? res.status(404).json({ message: "No User found with this ID!" })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
 },
 
-    toJSON:{
-        virtuals: true,
-    },
-    id: false,
-}
 
-); 
-
-userSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
-}); 
-
-const User = model ('User, userSchema');
-
-
-module.exports = User;
-
-
-
+};
